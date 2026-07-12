dd26afa33646f883afe80d56649b18caf81de697
Liam2109 <emoliam255@gmail.com>
2026-07-12 15:43:29 +0200

    fix prisma config for vercel

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,6 @@
-import { PrismaClient } from "@prisma/client"
+import path from "path"
+import { defineConfig } from "prisma/config"
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
-}
-
-export const prisma = global.prisma ?? new PrismaClient()
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma
+export default defineConfig({
+  schema: path.join("prisma", "schema.prisma"),
+})
